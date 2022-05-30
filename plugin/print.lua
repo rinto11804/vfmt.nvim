84d6913c25fff9cbff74581c2795a55437a7acd9
rinto11804 <rinto11804@gmail.com>
2022-05-30 07:36:12 +0530

    window

--- plugin/print.lua
+++
@@ -1,6 +0,0 @@
-local currentBuffer = vim.api.nvim_buf_get_name(1);
-local enter = "<cr>"
-vim.keymap.set('n', 'ff', '<cmd>!v fmt -w ' .. currentBuffer .. enter, {
-  silent = true,
-  buffer = 0
-});
